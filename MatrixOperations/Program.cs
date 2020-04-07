b76e61dce48fd4245c4bdfb1a8dd3425d9ddbbe4
Paulo Machado <paulo.machado@dextra-sw.com>
2020-04-07 12:24:09 -0300

    criação do projeto

---
+++ MatrixOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
